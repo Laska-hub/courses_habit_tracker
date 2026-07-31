5afc6ea0b048f0bb0f89e7a099b220e3f3c722c2
Olesia Laskovets <olesya_izvarina@mail.ru>
2026-07-31 23:13:10 +0300

    Configure Celery with Redis

--- config/settings.py
+++ config/settings.py
@@ -163,3 +163,15 @@ SWAGGER_SETTINGS = {
         }
     },
 }
+
+# Celery
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+
+CELERY_ACCEPT_CONTENT = [
+    "json",
+]
+
+CELERY_TASK_SERIALIZER = "json"
+
+CELERY_TIMEZONE = TIME_ZONE
